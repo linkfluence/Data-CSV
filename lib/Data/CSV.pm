b909627d1dbce13eedfcbf253f6cb8a454d57a27
Thomas Drugeon <tdrugeon@brochant.rtgi.eu>
2011-09-12 10:52:24 +0200

    new utility function flatten()

--- lib/Data/CSV.pm
+++ lib/Data/CSV.pm
@@ -1,6 +1,8 @@
 package Data::CSV;
 use strict;
 use warnings;
+use base 'Exporter';
+our @EXPORT_OK = qw(flatten);
 
 sub reader {
 	require Data::CSV::Reader;
@@ -21,5 +23,55 @@ sub read {
 sub write {
 	writer->all(@_)
 }
+
+# utility function, quick (15 times as fast) and dirty (only works for hashes and without circular ref detection) version of Hash::Flatten::flatten()
+{
+	my $res;
+	sub flatten {
+		my $hash = shift;
+		$res = {};
+		_flatten($hash, '');
+		return $res;
+	}
+	sub _flatten {
+		my ($hash, $path) = @_;
+		while (my ($k, $v) = each %$hash) {
+			if (ref $v) {
+				_flatten($v, "$path$k:")
+			} else {
+				$res->{$path.$k} = $v
+			}
+		}
+	}
+}
+
+#my $test = {
+#	a	=> 1,
+#	b	=> {
+#		c	=> 2,
+#		d	=> {
+#			e	=> 3,
+#			f	=> 4,
+#		}
+#	}
+#};
+#
+#use YAML;
+#use Time::This;
+#use Hash::Flatten ();
+#timed {
+#	for (1..100_000) {
+#		my $hash = Hash::Flatten::flatten($test);
+#	}
+#} "Hash::Flatten";
+#
+#timed {
+#	for (1..100_000) {
+#		my $hash = flatten($test);
+#	}
+#} __PACKAGE__;
+#
+#print Dump flatten($test);
+#exit;
 
 1
